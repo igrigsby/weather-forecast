7ff6273ed32af55763d9ab685e0d19a63fd7369f
Isaiah Grigsby <grigsbyisaiah@gmail.com>
2024-02-05 16:05:51 -0600

    Updated weather_forecast.py
    There's 2 main things this will do.
    
    1.) Make API request and process the response
    2.)  Displays header and tracks the unique dates

--- weather_forecast.py
+++ weather_forecast.py
@@ -12,4 +12,15 @@ def get_weather_forecast(zip_code):
     api_url = "https://api.openweathermap.org/data/2.5/forecast"
     params = {'zip': zip_code, 'appid': OPENWEATHERMAP_API_KEY}
 
+    try:
+        # Make API request
+        response = requests.get(api_url, params=params).json()
+        forecast_list = response['list']
+
+        # Display header
+        print("# Date         Temperature (°F)       Precipitation")
+
+        # Track unique dates to avoid duplicate entries
+        unique_dates = set()
+
 
