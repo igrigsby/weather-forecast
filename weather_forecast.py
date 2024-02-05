0d469c653ca320ecabcf157c20055b1ce868a970
Isaiah Grigsby <grigsbyisaiah@gmail.com>
2024-02-05 15:55:06 -0600

    Added temperature conversion function

--- weather_forecast.py
+++ weather_forecast.py
@@ -2,3 +2,9 @@ import requests
 from datetime import datetime
 
 from config import OPENWEATHERMAP_API_KEY
+
+def kelvin_to_fahrenheit(kelvin):
+    """Convert temperature from Kelvin to Fahrenheit."""
+    return (kelvin - 273.15) * 9/5 + 32
+
+
