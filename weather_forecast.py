2ff16620a8e3a5689d929f31a0995122fb4d2487
Isaiah Grigsby <grigsbyisaiah@gmail.com>
2024-02-05 15:55:54 -0600

    Added weather forecast retrieval function

--- weather_forecast.py
+++ weather_forecast.py
@@ -7,4 +7,9 @@ def kelvin_to_fahrenheit(kelvin):
     """Convert temperature from Kelvin to Fahrenheit."""
     return (kelvin - 273.15) * 9/5 + 32
 
+def get_weather_forecast(zip_code):
+    """Get 5-day weather forecast for the given zip code."""
+    api_url = "https://api.openweathermap.org/data/2.5/forecast"
+    params = {'zip': zip_code, 'appid': OPENWEATHERMAP_API_KEY}
+
 
